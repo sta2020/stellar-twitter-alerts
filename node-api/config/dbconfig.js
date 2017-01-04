eb055c897c61d02499bdefcf4cd95ba25e313328
sta2020 <sta2020@zoho.com>
2017-01-04 20:57:59 +0100

    Create dbconfig.js

---
+++ node-api/config/dbconfig.js
@@ -0,0 +1,10 @@
+//Input your own values
+var db = {
+		host     : 'HOSTNAME',
+    user     : 'USERNAME',
+    password : 'PASSWORD',
+    database : 'DATABASE',
+    charset  : 'CHARSET'
+  };
+
+module.exports  =  db;
