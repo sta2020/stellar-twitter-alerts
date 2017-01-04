e219cd93009e82152581500b0347ae8e9b15ea3c
sta2020 <sta2020@zoho.com>
2017-01-04 12:50:54 +0100

    Create front.js

---
+++ js/angular-app/controllers/front.js
@@ -0,0 +1,40 @@
+var stellarAlerts = angular.module('stellarAlerts');
+var baseUrl = 'https://stellaralerts.info:8081/';
+
+stellarAlerts.controller('frontController', function($scope, $state, $http) {
+	$scope.userData = {};
+	
+	$scope.init = function() {
+		
+
+	};
+
+	$scope.subscribe = function() {
+		// verify twitter acct
+		// verify stellar acct
+		// send to db
+		
+		console.log("userData", $scope.userData);
+		if (!StellarSdk.Keypair.isValidPublicKey($scope.userData.accountID)) {
+			alert("Invalid Account Id");
+			return false;
+		} else{
+			$http({
+                method: 'POST',
+                url: baseUrl+'subscribe',
+                headers: { 'Content-Type' : 'application/x-www-form-urlencoded'},
+                data: $.param($scope.userData)
+            })
+					.success(function(data) {
+						console.log(data);
+					})
+					.error(function(data) {
+						console.log(data);
+					});
+		}
+
+	};
+
+});
+
+
